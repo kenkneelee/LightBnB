3cee957a6515eee91b65ac94fd8978f9459594bf
Kenny Lee <kenny.lee927@gmail.com>
2023-10-05 05:18:22 -0400

    refactor getUserWithId to pull from db

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 
 const getUserWithEmail = (email) => {
   return pool
-    .query(`SELECT * FROM users WHERE users.email = $1`, [email])
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
     .then((result) => {
       return result.rows[0];
     })
@@ -31,8 +31,17 @@ const getUserWithEmail = (email) => {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+
+const getUserWithId = (id) => {
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
 };
 
 /**
